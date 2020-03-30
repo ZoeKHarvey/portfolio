ff558deb134295307fcca1667179d44c0dfbff20
Zoe <zoekharvey@gmail.com>
2020-03-30 16:36:57 -0400

    Last commit before trying image crossfade component

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Projects.scss';
+import CowboyAlienHome from '../../assets/cowboysaliens.png';
+import CowboysAliens2 from '../../assets/cowboysaliens2.png';
 
 const Projects = () => {
 
   return(
     <div className="Projects">
       <h1>Projects</h1>
+      <div>
+      <a href='https://edwindelbosque.github.io/Cowboys-vs-Aliens/'>
+        <img className="slides" src={CowboyAlienHome} />
+        <img className="slides" src={CowboysAliens2} />
+      </a>
+      <a href='https://github.com/ZoeKHarvey/Cowboys-vs-Aliens'>Github</a>
+      </div>
     </div>
   )
 }
