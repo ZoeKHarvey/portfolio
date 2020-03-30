b3ed269a43e41abc0f29183b26d6c4ce52699182
Zoe <zoekharvey@gmail.com>
2020-03-30 17:10:17 -0400

    Add gutenreader gif to projects page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -3,6 +3,7 @@ import './Projects.scss';
 import CowboyAlienHome from '../../assets/cowboysaliens.png';
 import CowboysAliens2 from '../../assets/cowboysaliens2.png';
 import CrossfadeImage from 'react-crossfade-image';
+import gutenGIF from '../../assets/gutenreader.gif';
 
 class Projects extends Component {
   constructor() {
@@ -24,7 +25,15 @@ render() {
   return(
     <div className="Projects">
       <h1>Projects</h1>
-      <div className="projects-div">
+
+      <div className="projects-div gutenreader-div">
+        <CrossfadeImage src={gutenGIF} />
+        {/* <img src={gutenGIF} /> */}
+      {/* <iframe src="https://user-images.githubusercontent.com/50784336/72040161-eea30180-3264-11ea-965d-b347b1b3fb7e.gif"></iframe> */}
+      </div>
+
+
+      <div className="projects-div cowboy-div">
         <button onClick={this.toggleImageSrc}>Next</button>
       <CrossfadeImage src={this.state.imageSrc} />
       <a href='https://edwindelbosque.github.io/Cowboys-vs-Aliens/'>
@@ -33,6 +42,8 @@ render() {
       </a>
       <a href='https://github.com/ZoeKHarvey/Cowboys-vs-Aliens'>Github</a>
       </div>
+
+
     </div>
   )
 }
