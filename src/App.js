e8b2f63f1a5d52d00a873465940361f8baa97e6e
Zoe <zoekharvey@gmail.com>
2020-03-30 13:19:32 -0400

    Create and define project component route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import Home from './Components/Home/Home';
-import Header from './Components/Header/Header'
-import './App.scss';
 import { Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Projects from './Components/Projects/Projects'
+import './App.scss';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Route exact path='/' render = {() => <> <Home /> </>} />
+      <Route exact path='/projects' render = {() => <> <Projects /> </>} />
     </div>
   );
 }
