ac0621fcd248ddfcdfc20571991729f914a18d18
Zoe <zoekharvey@gmail.com>
2020-03-31 17:50:44 -0400

    Improve code readability on projects component

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -17,7 +17,8 @@ class Projects extends Component {
       imageSrc1: gutenHome,
       imageSrc2: CowboyAlienHome,
       cinemaPics: cinemaHome,
-      gutenTextVisible: 'hidden'
+      gutenTextVisible: 'hidden',
+
     }
   }
 
@@ -43,58 +44,50 @@ render() {
 
   return(
     <div className="Projects">
-
-      <div className="projects-div gutenreader-div">
-
-      <div className="gutenpics" onClick={() => this.toggleVisibility('guten')}>
-        <CrossfadeImage src={this.state.imageSrc1} />
-        <p style={{visibility: this.state.gutenTextVisible}} className="projects-p-guten">GutenReader provides the user with a cinematic experience. It is an eReader built on the Project Gutenberg API that performs sentiment analysis (with IBM Watson) to determine the mood of the current page and plays music (with Spotify) that matches that mood. Designed with accessibility in mind!</p>
+      <div className="projects-div gutenreader-div" style={this.state.gutenStyle}>
+        <div className="gutenpics" onClick={() => this.toggleVisibility('guten')}>
+          <CrossfadeImage src={this.state.imageSrc1} />
+          <p style={{visibility: this.state.gutenTextVisible}} className="projects-p-guten">GutenReader provides the user with a cinematic experience. It is an eReader built on the Project Gutenberg API that performs sentiment analysis (with IBM Watson) to determine the mood of the current page and plays music (with Spotify) that matches that mood. Designed with accessibility in mind!</p>
         </div>
-        <div className="projects-div-footer">
-          <a href="https://github.com/ZoeKHarvey/guten_reader_FE">Github</a>
-          <a href="https://www.youtube.com/watch?v=OcsrT65ifnE">Demo</a>
-          <svg className="svg-next" onClick={ () => this.toggleImageSrc(1) } xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-        </div>
+      <div className="projects-div-footer">
+        <a href="https://github.com/ZoeKHarvey/guten_reader_FE">Github</a>
+        <a href="https://www.youtube.com/watch?v=OcsrT65ifnE">Demo</a>
+        <svg className="svg-next" onClick={ () => this.toggleImageSrc(1) } xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
       </div>
+    </div>
 
-      <div className="projects-div iced-div">
-        <div className="icedpics">
-          <CrossfadeImage src={icedhome} />
-        </div>
-        <div className="projects-div-footer">
+    <div className="projects-div iced-div">
+      <div className="icedpics">
+        <CrossfadeImage src={icedhome} />
+      </div>
+      <div className="projects-div-footer">
         <a href="https://github.com/ZoeKHarvey/stats-on-stats">Github</a>
         <svg className="svg-next" onClick={ () => this.toggleImageSrc(3) } xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-        </div>
       </div>
+    </div>
 
-
-      <div className="projects-div cowboy-div">
-        <div className="cowboypics">
-      <CrossfadeImage src={this.state.imageSrc2} />
+    <div className="projects-div cowboy-div">
+      <div className="cowboypics">
+        <CrossfadeImage src={this.state.imageSrc2} />
       </div>
-
       <div className="projects-div-footer">
-      <a href='https://github.com/ZoeKHarvey/Cowboys-vs-Aliens'>Github</a>
-      <a href='https://edwindelbosque.github.io/Cowboys-vs-Aliens/'>
+        <a href='https://github.com/ZoeKHarvey/Cowboys-vs-Aliens'>Github</a>
+        <a href='https://edwindelbosque.github.io/Cowboys-vs-Aliens/'>
           Live
-      </a>
-      <svg className="svg-next" onClick={ () => this.toggleImageSrc(2) } xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-
+        </a>
+        <svg className="svg-next" onClick={ () => this.toggleImageSrc(2) } xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
       </div>
-      </div>
+    </div>
 
-      <div className="projects-div cinema-div">
-        <div className="cinemapics">
+    <div className="projects-div cinema-div">
+      <div className="cinemapics">
         <CrossfadeImage src={this.state.cinemaPics} />
-        </div>
-
-        <div className="projects-div-footer">
-          <a href="https://github.com/SamuelColeman/cinema-night">Github</a>
-        <svg className="svg-next" onClick={ () => this.toggleImageSrc(4) } xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-
-        </div>
       </div>
-
+      <div className="projects-div-footer">
+        <a href="https://github.com/SamuelColeman/cinema-night">Github</a>
+        <svg className="svg-next" onClick={ () => this.toggleImageSrc(4) } xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+      </div>
+    </div>
 
     </div>
   )
