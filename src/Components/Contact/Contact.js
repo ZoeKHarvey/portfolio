6ac81af98a70c678eeaefdaba0e7f0b2cc1ce2f6
Zoe <zoekharvey@gmail.com>
2020-04-01 17:09:08 -0400

    Add phone and email icons on contact component, hover number on phone

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Contact.scss';
+import mail from '../../assets/mail.svg';
+import phone from '../../assets/phone.svg'
 
 const Contact = () => {
 
   return(
     <div className="Contact">
-      <h1>Contact</h1>
-      <a href="mailto:zoekharvey@gmail.com">Send Email</a>
+      <a title="802-343-4999" href="tel:802-343-4999">
+        <img src={phone} />
+      </a>
+      <a href="mailto:zoekharvey@gmail.com">
+        <img src={mail} />
+      </a>
     </div>
   )
 }
