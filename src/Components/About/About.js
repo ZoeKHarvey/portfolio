a799fc720a33b354443ab1e3efdff1a102d33312
Zoe <zoekharvey@gmail.com>
2020-04-09 12:09:52 -0400

    Add test dropdown

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './About.scss';
 import Footer from '../Footer/Footer'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 const About = () => {
   let technologies = [ 
@@ -22,6 +24,11 @@ const About = () => {
     return <li>{tech.name}</li>
   })
 
+  const options = [
+    'one', 'two', 'three'
+  ];
+  const defaultOption = options[0];
+
   return(
     <div className="About">
       <h6>Web Desinger</h6>
@@ -31,6 +38,11 @@ const About = () => {
         <ul>
           {mappedTechNames}
         </ul>
+
+
+ 
+
+<Dropdown options={options} placeholder="Sort By" />;
       </section>
 
       <Footer />
