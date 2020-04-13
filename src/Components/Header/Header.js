ca379c7aeb9eefbe7e2ec85c876fcbe335199305
Zoe <zoekharvey@gmail.com>
2020-04-12 21:06:56 -0400

    Clean up Header js code

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,51 +1,39 @@
 import React from 'react';
 import './Header.scss';
-import {Link, NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
 
-
   return(
-
     <div className="Header">
       <Link to='/'>
-      <span className="header-logo">
-      </span>
-
+        <span className="header-logo"></span>
       </Link>
-<div className="Header2">
-   
-      <Link to='/projects'>
-        <div className="header">
-          <p className="header-tab">Projects & Portfolio</p>
-        </div>
-      </Link>
-      
-      <Link to='/about'>
-        <div className="header">
-          <p className="header-tab">About Me</p>
-        </div>
-      </Link>
-
-      <Link to='/resume'>
-        <div className="header">
-          <p className="header-tab">Resume</p>
-        </div>
-      </Link>
-
-      <Link to='/contact'>
-        <div className="header">
-          <p className="header-tab">Contact</p>
-        </div>
-      </Link>
+      <div className="Header2">
+        <Link to='/projects'>
+          <div className="header">
+            <p className="header-tab">Projects & Portfolio</p>
+          </div>
+        </Link>
+        <Link to='/about'>
+          <div className="header">
+            <p className="header-tab">About Me</p>
+          </div>
+        </Link>
+        <Link to='/resume'>
+          <div className="header">
+            <p className="header-tab">Resume</p>
+          </div>
+        </Link>
+        <Link to='/contact'>
+          <div className="header">
+            <p className="header-tab">Contact</p>
+          </div>
+        </Link>
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Header;
 
-
-{/* <div class="example">
-<span class="hover hover-1">This underline goes left to right</span>
-</div> */}
