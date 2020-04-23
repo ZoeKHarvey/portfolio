27eff073142394641c4db5b6e983b2cd7497b565
Zoe <zoekharvey@gmail.com>
2020-04-23 14:17:21 -0400

    Updates

---
+++ precache-manifest.0bc8f30f982017715d5c7de6b0f3bbd3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d260616395e91cedadc6de8d828dbe",
+    "url": "/ZoeKHarvey/portfolio/index.html"
+  },
+  {
+    "revision": "2c831e396ef33b7a9498",
+    "url": "/ZoeKHarvey/portfolio/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "2f7533d580c6e0f06922",
+    "url": "/ZoeKHarvey/portfolio/static/css/main.3aabfc6a.chunk.css"
+  },
+  {
+    "revision": "2c831e396ef33b7a9498",
+    "url": "/ZoeKHarvey/portfolio/static/js/2.d2a1bae0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ZoeKHarvey/portfolio/static/js/2.d2a1bae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7533d580c6e0f06922",
+    "url": "/ZoeKHarvey/portfolio/static/js/main.50571230.chunk.js"
+  },
+  {
+    "revision": "ee74b448d116b0d052d7",
+    "url": "/ZoeKHarvey/portfolio/static/js/runtime-main.8b7377c9.js"
+  },
+  {
+    "revision": "abf9a1c4cedb6b1c324b0da413d686e5",
+    "url": "/ZoeKHarvey/portfolio/static/media/cinema-home.abf9a1c4.png"
+  },
+  {
+    "revision": "d532424f7d14d4b43a5be37e566b882e",
+    "url": "/ZoeKHarvey/portfolio/static/media/cinema-login.d532424f.png"
+  },
+  {
+    "revision": "dc5695542295cbe63d6239b1dd84e236",
+    "url": "/ZoeKHarvey/portfolio/static/media/cowboysaliens.dc569554.png"
+  },
+  {
+    "revision": "3b1a141bce766a1357b8dd185ea3f8f3",
+    "url": "/ZoeKHarvey/portfolio/static/media/cowboysaliens2.3b1a141b.png"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/ZoeKHarvey/portfolio/static/media/github-image.192a6620.svg"
+  },
+  {
+    "revision": "f709ab6d4999ee4e7d52a0af5b38d7f0",
+    "url": "/ZoeKHarvey/portfolio/static/media/gutenhome.f709ab6d.png"
+  },
+  {
+    "revision": "094b88587514c8f168e32cc62c75a222",
+    "url": "/ZoeKHarvey/portfolio/static/media/gutenreader.094b8858.gif"
+  },
+  {
+    "revision": "ef60dcb1e1850deaf9f45cc9a69ffb82",
+    "url": "/ZoeKHarvey/portfolio/static/media/harvey-zoe.ef60dcb1.pdf"
+  },
+  {
+    "revision": "a08f7ac2cc51e3786e782533be72442f",
+    "url": "/ZoeKHarvey/portfolio/static/media/icedhome.a08f7ac2.png"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/ZoeKHarvey/portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "7c86c6ea4a23d1747b756e58de3d3973",
+    "url": "/ZoeKHarvey/portfolio/static/media/mail.7c86c6ea.svg"
+  },
+  {
+    "revision": "f3b7d629c80a85788fe1cc7377576fec",
+    "url": "/ZoeKHarvey/portfolio/static/media/phone.f3b7d629.svg"
+  },
+  {
+    "revision": "2fcbac0dab93ef9aa7f76a4a450374ca",
+    "url": "/ZoeKHarvey/portfolio/static/media/turing-logo.2fcbac0d.svg"
+  }
+]);
