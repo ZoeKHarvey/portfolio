b0e51e8af35f33f665798fd7b8fcc70ea3bf6be0
Zoe <zoekharvey@gmail.com>
2020-05-28 16:40:16 -0500

    Updates

---
+++ precache-manifest.28456c4a482b79f79fa92edff6518768.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5884093b06bcf1e180b7c39d50273fe1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b10cce8ac2354102e7be",
+    "url": "/portfolio/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "ef468e933f272ba698e8",
+    "url": "/portfolio/static/css/main.58b942d2.chunk.css"
+  },
+  {
+    "revision": "b10cce8ac2354102e7be",
+    "url": "/portfolio/static/js/2.d2eaa44c.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/portfolio/static/js/2.d2eaa44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef468e933f272ba698e8",
+    "url": "/portfolio/static/js/main.5df5eff1.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "abf9a1c4cedb6b1c324b0da413d686e5",
+    "url": "/portfolio/static/media/cinema-home.abf9a1c4.png"
+  },
+  {
+    "revision": "d532424f7d14d4b43a5be37e566b882e",
+    "url": "/portfolio/static/media/cinema-login.d532424f.png"
+  },
+  {
+    "revision": "dc5695542295cbe63d6239b1dd84e236",
+    "url": "/portfolio/static/media/cowboysaliens.dc569554.png"
+  },
+  {
+    "revision": "3b1a141bce766a1357b8dd185ea3f8f3",
+    "url": "/portfolio/static/media/cowboysaliens2.3b1a141b.png"
+  },
+  {
+    "revision": "e3083f3ce436c97dbf2eec2b75eed1d0",
+    "url": "/portfolio/static/media/cycle-east.e3083f3c.png"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/portfolio/static/media/github-image.192a6620.svg"
+  },
+  {
+    "revision": "f709ab6d4999ee4e7d52a0af5b38d7f0",
+    "url": "/portfolio/static/media/gutenhome.f709ab6d.png"
+  },
+  {
+    "revision": "094b88587514c8f168e32cc62c75a222",
+    "url": "/portfolio/static/media/gutenreader.094b8858.gif"
+  },
+  {
+    "revision": "ef60dcb1e1850deaf9f45cc9a69ffb82",
+    "url": "/portfolio/static/media/harvey-zoe.ef60dcb1.pdf"
+  },
+  {
+    "revision": "a08f7ac2cc51e3786e782533be72442f",
+    "url": "/portfolio/static/media/icedhome.a08f7ac2.png"
+  },
+  {
+    "revision": "abd0655c50d48236130e11de5a7da9c6",
+    "url": "/portfolio/static/media/idea-box.abd0655c.png"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "53c775534be958dfacbab2e135c1731b",
+    "url": "/portfolio/static/media/logo.53c77553.png"
+  },
+  {
+    "revision": "7c86c6ea4a23d1747b756e58de3d3973",
+    "url": "/portfolio/static/media/mail.7c86c6ea.svg"
+  },
+  {
+    "revision": "55f6849429de279d473c459c7ead3249",
+    "url": "/portfolio/static/media/menu-24px.55f68494.svg"
+  },
+  {
+    "revision": "f3b7d629c80a85788fe1cc7377576fec",
+    "url": "/portfolio/static/media/phone.f3b7d629.svg"
+  },
+  {
+    "revision": "969cded346a6a2105dba0b38bd195961",
+    "url": "/portfolio/static/media/swapi-home.969cded3.png"
+  },
+  {
+    "revision": "2fcbac0dab93ef9aa7f76a4a450374ca",
+    "url": "/portfolio/static/media/turing-logo.2fcbac0d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2c040c901b80b0d338971e4ab6c21ab5.js"
+  "/portfolio/precache-manifest.28456c4a482b79f79fa92edff6518768.js"
 );
 
 self.addEventListener('message', (event) => {
