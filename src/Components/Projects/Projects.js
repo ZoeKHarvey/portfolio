0e5b334f1eaa9520d7b064e5581b63c4d2f97fb9
Zoe <zoekharvey@gmail.com>
2020-06-03 18:43:09 -0500

    Finish styling info boxes

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -41,18 +41,21 @@ class Projects extends Component {
             <a target="_blank" href="https://github.com/ZoeKHarvey/guten_reader_FE">Github</a>
             <a target="_blank" href="https://www.youtube.com/watch?v=OcsrT65ifnE">Demo</a>
             <Popup trigger={
-              <div className="div-info guten-info">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
-              </div>}
+              
+                <svg className="guten-info-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="25px" height="25px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+              }
             position="top right">  
               <div className="about-p">
                 <p>
-                  <span className="info-span">GutenReader</span><br/>
+                  <span>GutenReader</span><br/>
                   An eReader built on the Project Gutenberg API that performs sentiment analysis (with IBM Watson) to determine the mood of the current page and plays music (with Spotify) that matches that mood. <br/><br/> 
-              Designed with accessibility in mind!<br/><br/>
-              Built With: <br/>
-              Front End - React Native / Jest / Enzyme <br/>
-              Backend - Rails / Sinatra / RSpec / unittest</p>
+              Designed with accessibility in mind!<br/>
+              
+              </p>
+              <p className="info-tech">
+              React Native <span>/</span> Jest <span>/</span> Enzyme <br/>
+              Rails <span>/</span> Sinatra <span>/</span> RSpec <span>/</span> unittest
+              </p>
               </div>
             </Popup>
             {/* <svg className="svg-next" onClick={ () => this.toggleImageSrc(1) } xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/><path d="M0 0h24v24H0z" fill="none"/></svg> */}
@@ -69,19 +72,19 @@ class Projects extends Component {
             <a target="_blank" href="https://github.com/ZoeKHarvey/stats-on-stats">Github</a>
             <Popup trigger={
               <div className="div-info">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="25px" height="25px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
             position="top right">  
               <div className="about-p">
                 <p>
                 <span className="info-span">Iced</span><br/>
                   Iced is built on an unofficial NHL API to deliver team and player's statistics. Star your favorite players
-                  as well keep up to date on team schedules. <br/> <br/>
-                  Built With: <br/>
-                  React / Redux / SCSS <br/>
-                  Tested With: <br/>
-                  Jest / Enzyme
+                  as well keep up to date on team schedules. <br/> 
                 </p>
+                  <p className="info-tech">
+                  React <span>/</span> Redux <span>/</span> SCSS <br/>
+                  Jest <span>/</span> Enzyme
+                  </p>
               </div>  
             </Popup>
           </div>
@@ -95,15 +98,17 @@ class Projects extends Component {
             <a target="_blank" href=''>Github</a>
             <Popup trigger={
               <div className="div-info">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="25px" height="25px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
             position="top right">  
               <div className="about-p">
                 <p>
-                <span className="info-span">Cycle East</span><br/>
+                <span>Cycle East</span><br/>
                   A website built for a new local bike tour company. Worked closely with potential users
                   to design a pleasing UI that is targetted to pique interest and inform bikers on this unique experience. <br/> <br/>
-                  Built With: React / SCSS
+                </p>
+                <p className="info-tech">
+                  React <span>/</span> SCSS
                 </p>
               </div>
             </Popup>         
@@ -119,16 +124,18 @@ class Projects extends Component {
             <a target="_blank" href='https://edwindelbosque.github.io/Cowboys-vs-Aliens/'>Live</a>
             <Popup trigger={
               <div className="div-info">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="25px" height="25px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
             position="top right">  
               <div className="about-p">
                 <p>
-                <span className="info-span">Cowboys vs Aliens</span><br/>
+                <span>Cowboys vs Aliens</span><br/>
                   Based on the popular game show Family Feud, this app allows 2 users to play along. 
-                  It keeps track of the users' scores for each unique round including a timed challenge round. <br/><br/>
-                  Built With: Javascript / HTML / SASS <br/>
-                  Tested With: Mocha / Chai & Spies
+                  It keeps track of the users' scores for each unique round including a timed challenge round. <br/>
+                </p>
+                <p className="info-tech">
+                  Javascript <span>/</span> HTML <span>/</span> SASS <br/>
+                  Mocha <span>/</span> Chai & Spies
                 </p>
               </div>
             </Popup>
@@ -145,7 +152,7 @@ class Projects extends Component {
             <a target="_blank" href="https://github.com/ZoeKHarvey/swapi-box">Github</a>
             <Popup trigger={
               <div className="div-info">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="25px" height="25px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
             position="top right">  
               <div className="about-p">
@@ -153,10 +160,13 @@ class Projects extends Component {
                 <span className="info-span">SWAPI Box</span><br/>
                   Star Wars fans unite! This app is built with the Star Wars Api to bring all of your favorite
                   characters and spacecrafts together. Learn about each movie, star your favorite characters to keep
-                  them organized and more! <br/><br/>
-                  Built With: React / SASS <br/>
-                  Tested With: Jest / Enzyme
+                  them organized and more! <br/>
                 </p>
+                <p className="info-tech">
+                  React <span>/</span> SASS <span>/</span>Jest <span>/</span> Enzyme
+
+                </p>
+
               </div>
             </Popup>          </div>
         </div>
@@ -169,7 +179,7 @@ class Projects extends Component {
             <a target="_blank" href="https://github.com/SamuelColeman/cinema-night">Github</a>
             <Popup trigger={
               <div className="div-info">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="25px" height="25px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
             position="top right">  
               <div className="about-p">
@@ -177,9 +187,10 @@ class Projects extends Component {
                 <span className="info-span">Cinema Night</span><br/>
                   An app that allows users to browse the top 20 newly released 
                   films of this year. The user is able to read the description of each film and once 
-                  logged in can add them to their favourites list <br/><br/>
-                  Built With: React/Redux <br/>
-                  Tested With: Jest / Enzyme
+                  logged in can add them to their favourites list <br/>
+                  </p>
+                  <p className="info-tech">
+                  React<span>/</span>Redux <span>/</span>Jest <span>/</span> Enzyme
                   </p>
               </div>
             </Popup>
@@ -195,7 +206,7 @@ class Projects extends Component {
             <a target="_blank" href="https://techfluent.io">Site</a>
             <Popup trigger={
               <div className="div-info">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="25px" height="25px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
             position="top right">  
               <div className="about-p">
