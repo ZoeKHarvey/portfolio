d6b4fb820565f95cced335d1b379c48a69958f15
Zoe <zoekharvey@gmail.com>
2020-06-03 18:03:19 -0500

    Fix info popup positions

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -14,7 +14,6 @@ import cycleHome from '../../assets/cycle-east.png';
 import swapiHome from '../../assets/swapi-home.png';
 import techfluent from '../../assets/tech-fluent.png';
 
-
 class Projects extends Component {
   constructor() {
     super()
@@ -22,21 +21,10 @@ class Projects extends Component {
       imageSrc1: gutenHome,
       imageSrc2: CowboyAlienHome,
       cinemaPics: cinemaHome,
-      gutenTextVisible: 'hidden',
     };
   };
 
-  toggleImageSrc = (srcId) => {
-    if(srcId === 1) {
-      this.state.imageSrc1 === gutenHome ? this.setState({ imageSrc1: gutenGIF }) : this.setState({ imageSrc1: gutenHome })
-    };
-    if(srcId === 2) {
-      this.state.imageSrc2 === CowboyAlienHome ? this.setState({ imageSrc2: CowboysAliens2 }) : this.setState({ imageSrc2: CowboyAlienHome })
-    };
-    if(srcId === 4) {
-      this.state.cinemaPics === cinemaHome ? this.setState({ cinemaPics: cinemaLogin }) : this.setState({ cinemaPics: cinemaHome})
-    };
-  };
+
 
 
   render() {
@@ -46,7 +34,6 @@ class Projects extends Component {
         <div className="projects-grid">
 
         <div className="projects-div gutenreader-div" style={this.state.gutenStyle}>
-          {/* <div className="gutenpics"> */}
             <img className="gutenpics" src={this.state.imageSrc1} />
            
     
@@ -82,7 +69,7 @@ class Projects extends Component {
               <div className="div-info">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
-            position="center left">  
+            position="top right">  
               <div className="about-p">
                 <p>Iced is built on an unofficial NHL API to deliver team and player's statistics. Star your favorite players
                   as well keep up to date on team schedules. <br/> <br/>
@@ -106,7 +93,7 @@ class Projects extends Component {
               <div className="div-info">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
-            position="center left">  
+            position="top right">  
               <div className="about-p">
                 <p>A website built for a new local bike tour company. Worked closely with potential users
                   to design a pleasing UI that is targetted to pique interest and inform bikers on this unique experience. <br/> <br/>
@@ -128,7 +115,7 @@ class Projects extends Component {
               <div className="div-info">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
-            position="center left">  
+            position="top right">  
               <div className="about-p">
                 <p>Based on the popular game show Family Feud, this app allows 2 users to play along. 
                   It keeps track of the users' scores for each unique round including a timed challenge round. <br/><br/>
@@ -137,7 +124,6 @@ class Projects extends Component {
                 </p>
               </div>
             </Popup>
-            <svg className="svg-next" onClick={ () => this.toggleImageSrc(2) } xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
 
           </div>
         </div>
@@ -153,7 +139,7 @@ class Projects extends Component {
               <div className="div-info">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
-            position="center right">  
+            position="top right">  
               <div className="about-p">
                 <p>Star Wars fans unite! This app is built with the Star Wars Api to bring all of your favorite
                   characters and spacecrafts together. Learn about each movie, star your favorite characters to keep
@@ -175,7 +161,7 @@ class Projects extends Component {
               <div className="div-info">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
-            position="center right">  
+            position="top right">  
               <div className="about-p">
                 <p>Cinema Night is an app that allows users to browse the top 20 newly released 
                   films of this year. The user is able to read the description of each film and once 
@@ -194,12 +180,12 @@ class Projects extends Component {
             <CrossfadeImage src={techfluent} />
           </div>
           <div className="projects-div-footer">
-            <a target="_blank" href="https://github.com/ZoeKHarvey/Idea-Box">Github</a>
+            <a target="_blank" href="https://techfluent.io">Site</a>
             <Popup trigger={
               <div className="div-info">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
               </div>}
-            position="center left">  
+            position="top right">  
               <div className="about-p">
                 <p>TechFluent delivers coding classes for spanish-speaking learners. The website
                   was designed and built focusing on minimalistic design and easy-to-navigate UI.
